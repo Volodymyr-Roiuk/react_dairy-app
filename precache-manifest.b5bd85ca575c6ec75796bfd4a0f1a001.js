d8add32267ac0af6d63f4e09a246a7a140d36d9a
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-17 00:04:40 +0300

    Updates

---
+++ precache-manifest.b5bd85ca575c6ec75796bfd4a0f1a001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23dd4e9e2ccf23a9db167e8cd3102503",
+    "url": "/react_dairy-app/index.html"
+  },
+  {
+    "revision": "81f9d2163c027da0b41a",
+    "url": "/react_dairy-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b1ed685500cd978a86ac",
+    "url": "/react_dairy-app/static/js/2.2072c233.chunk.js"
+  },
+  {
+    "revision": "81f9d2163c027da0b41a",
+    "url": "/react_dairy-app/static/js/main.2ed3133d.chunk.js"
+  },
+  {
+    "revision": "ef7397ae771429348f97",
+    "url": "/react_dairy-app/static/js/runtime-main.49668214.js"
+  }
+]);
---
+++ static/js/main.2ed3133d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact_dairy-app"]=this["webpackJsonpreact_dairy-app"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(2),p=n.n(c);n(8);var i=function(){return r.a.createElement("div",{className:"App"})};p.a.render(r.a.createElement(i,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.2ed3133d.chunk.js.map
